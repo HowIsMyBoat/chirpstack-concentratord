d8f9eae3b5b57ce9a0d60d6384a215abfa30bc69
Michael Herman <michael@sprybts.com>
2021-08-02 05:14:09 +0000

    Adding entrypoint for sx1301 models.

---
+++ himb/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [ -z $GW_MODE ];then
+  echo "Radio model not set. Exiting."
+  exit 1
+fi
+
+case "${GW_MODEL}" in
+  sx1301)
+    /app/bin/chirpstack-concentratord-sx1301 -c /app/conf/global.toml -c /app/conf/channels.toml -c /app/conf/band.toml -c /app/conf/gateway.toml
+    ;;
+  sx1302)
+    /app/bin/chirpstack-concentratord-sx1302 -c /app/conf/global.toml -c /app/conf/channels.toml -c /app/conf/band.toml -c /app/conf/gateway.toml
+    ;;
+  *)
+    echo "Undefined radio model. Exiting."
+    exit 2
+    ;;
+esac
+
+exit $?
